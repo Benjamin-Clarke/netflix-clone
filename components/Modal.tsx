2ff15574a206cec145a10539b58b9160e3740ce6
Ben <101375518+Benjamin-Clarke@users.noreply.github.com>
2023-09-04 12:19:00 +1000

    Finished Modal

--- components/Modal.tsx
+++ components/Modal.tsx
@@ -1,11 +1,11 @@
 import { modalState, movieState } from "@/atoms/modalAtom"
-import { PlusIcon, XMarkIcon } from "@heroicons/react/20/solid"
+import { HandThumbUpIcon, PlusIcon, XMarkIcon } from "@heroicons/react/20/solid"
 import MuiModal from "@mui/material/Modal"
 import { useEffect, useState } from "react"
 import { useRecoilState, useRecoilValue } from "recoil"
 import { Element, Genre } from "@/typings"
 import ReactPlayer from "react-player/lazy"
-import { FaPlay } from "react-icons/fa"
+import { FaPlay, FaVolumeOff, FaVolumeUp } from "react-icons/fa"
 
 export default function Modal() {
     const [showModal, setShowModal] = useRecoilState(modalState)
@@ -45,7 +45,7 @@ export default function Modal() {
     return (
         <MuiModal 
             open={showModal} onClose={handleClose}
-            className="fixed !top-7 left-0 right-0 z-50 mx-auto w-full max-w-5xl
+            className="fixed !top-7 left-0 right-0 z-50 mx-auto w-full max-w-4xl
             overflow-hidden overflow-y-scroll rounded-md scrollbar-hide"
         >
             <>
@@ -64,7 +64,7 @@ export default function Modal() {
                         height="100%"
                         style={{ position: 'absolute', top: '0', left: '0' }}
                         playing
-                        //muted={muted}
+                        muted={muted}
                     />
                     <div className="absolute bottom-10 flex w-full items-center justify-between
                     px-10">
@@ -74,9 +74,58 @@ export default function Modal() {
                                 <FaPlay className="h-7 w-7 text-black"/>
                                 Play
                             </button>
-                            <button>
-                                <PlusIcon/>
+                            <button className="modalButton">
+                                <PlusIcon className="h-7 w-7"/>
                             </button>
+                            <button className="modalButton">
+                                <HandThumbUpIcon className="h-7 w-7"/>
+                            </button>
+                        </div>
+                        <button 
+                            className="modalButton" 
+                            onClick={() => setMuted(!muted)}
+                        >
+                            {muted ? (
+                                <FaVolumeOff className="h-6 w-6"/>
+                            ) : (
+                                <FaVolumeUp className="h-6 w-6"/>
+                            )}
+                        </button>
+                    </div>
+                </div>
+                <div className="flex space-x-16 rounded-b-md bg-[#181818] px-10 py-8">
+                    <div className="space-y-6 text-lg">
+                        <div className="flex items-center space-x-2 text-sm">
+                            <p className="font-semibold text-green-400">
+                                {(movie!.vote_average * 10).toFixed(0)}% Match
+                            </p>
+                            <p className="font-light">
+                                {movie?.release_date || movie?.first_air_date}
+                            </p>
+                            <div className="flex h-4 items-center justify-center rounded border
+                            border-white/40 px-1.5 text-xs">
+                                HD
+                            </div>
+                        </div>
+                        <div className="flex flex-col gap-x-10 gap-y-4 font-light
+                        md:flex-row">
+                            <p className="w-5/6">
+                                {movie?.overview}
+                            </p>
+                            <div className="flex flex-col space-y-3 text-sm">
+                                <div>
+                                    <span className="text-[gray]">Genres: </span>
+                                    {genres.map((genre) => genre.name).join(', ')}
+                                </div>
+                                <div>
+                                    <span className="text-[gray]">Original language: </span>
+                                    {movie?.original_language}
+                                </div>
+                                <div>
+                                    <span className="text-[gray]">Total Votes: </span>
+                                    {movie?.vote_count}
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
