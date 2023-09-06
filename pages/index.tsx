062a5f06e0fd5644efa5af9b534e25ec39f7f74c
Ben <101375518+Benjamin-Clarke@users.noreply.github.com>
2023-09-06 14:47:42 +1000

    Working on Plans component

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import { modalState } from '@/atoms/modalAtom'
 import Banner from '@/components/Banner'
 import Header from '@/components/Header'
 import Modal from '@/components/Modal'
+import Plans from '@/components/Plans'
 import Row from '@/components/Row'
 import useAuth from '@/hooks/useAuth'
 import { Movie } from '@/typings'
@@ -32,12 +33,19 @@ export default function Home({
 } : Props) {
   const {loading} = useAuth()
   const showModal = useRecoilValue(modalState)
+  const subscription = false
 
-  if (loading) return null
+  if (loading || subscription === null) return null
+
+  if (!subscription) return (
+    <Plans/>
+  )
 
   return (
-    <div className='relative h-screen bg-gradient-to-b
-    from-gray-900/10 to-[#010511] lg:h-[140vh]'>
+    <div className={`relative h-screen bg-gradient-to-b
+    from-gray-900/10 to-[#010511] lg:h-[140vh] 
+    ${showModal && '!h-screen overflow-hidden'}`}
+    >
       <Head>
         <title>Home - Netflix</title>
         <link rel="icon" href="/favicon.ico" />
